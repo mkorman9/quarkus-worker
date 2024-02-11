381cf5b24f4e5b40f3a765d76ecdd4621c508481
Michal Korman <michal.korman@icloud.com>
2024-02-11 13:47:19 +0100

    replace vertx verticle with custom virtual thread

--- src/main/kotlin/com/github/mkorman9/TickLoopWorkerLifecycle.kt
+++ src/main/kotlin/com/github/mkorman9/TickLoopWorkerLifecycle.kt
@@ -2,47 +2,36 @@ package com.github.mkorman9
 
 import io.quarkus.runtime.ShutdownEvent
 import io.quarkus.runtime.StartupEvent
-import io.vertx.core.AbstractVerticle
-import io.vertx.core.DeploymentOptions
-import io.vertx.core.ThreadingModel
-import io.vertx.core.Vertx
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.event.Observes
 import org.jboss.logging.Logger
-import java.util.concurrent.SynchronousQueue
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.atomic.AtomicBoolean
+import java.time.Duration
 import kotlin.math.abs
 
 private const val TICK_INTERVAL: Long = 1000
 private const val SHUTDOWN_TIMEOUT: Long = 2500
 
 @ApplicationScoped
-class TickLoopWorkerVerticle(
+class TickLoopWorkerLifecycle(
     private val log: Logger,
-    private val vertxInstance: Vertx,
     private val tickLoopWorker: TickLoopWorker
-) : AbstractVerticle() {
-    private var isRunning = AtomicBoolean(true)
-    private val shutdownQueue = SynchronousQueue<Boolean>()
+) : Runnable {
+    private lateinit var thread: Thread
+    private var isRunning = true
 
     fun onStartup(@Observes startupEvent: StartupEvent) {
-        vertxInstance.deployVerticle(
-            this,
-            DeploymentOptions()
-                .setThreadingModel(ThreadingModel.VIRTUAL_THREAD)
-        )
+        thread = Thread.ofVirtual()
+            .name("tick-loop-thread")
+            .start(this)
     }
 
     fun onShutdown(@Observes shutdownEvent: ShutdownEvent) {
-        val wasRunning = isRunning.getAndSet(false)
-        if (wasRunning) {
-            shutdownQueue.poll(SHUTDOWN_TIMEOUT, TimeUnit.MILLISECONDS)
-        }
+        isRunning = false
+        thread.join(Duration.ofMillis(SHUTDOWN_TIMEOUT))
     }
 
-    override fun start() {
-        while (isRunning.get()) {
+    override fun run() {
+        while (isRunning) {
             val tickStartTime = System.currentTimeMillis()
 
             try {
@@ -59,14 +48,11 @@ class TickLoopWorkerVerticle(
                     Thread.sleep(timeUntilNextTick)
                 } catch (e: InterruptedException) {
                     log.error("Tick thread has been interrupted unexpectedly")
-                    isRunning.set(false)
                     return
                 }
             } else {
                 log.info("Tick is lagging behind ${abs(timeUntilNextTick)} ms")
             }
         }
-
-        shutdownQueue.offer(true)
     }
 }
